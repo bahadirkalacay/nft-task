db060525451385e6a9898ba45b201f842d26cada
Bahadır Kalaçay <bahadirkalacay@gmail.com>
2022-12-13 01:28:42 +0300

    scroll

--- src/components/ScroolButtons/ScrollButtons.js
+++ src/components/ScroolButtons/ScrollButtons.js
@@ -1,32 +1,60 @@
+import { useState } from "react";
 import { FaArrowCircleUp, FaArrowCircleDown } from "react-icons/fa";
-import "./style.css"
+import "./style.css";
 
 const ScrollButtons = () => {
-
+  const [visiable, setVisible] = useState(false);
   const downScroll = () => {
     window.scroll({
       top: document.body.offsetHeight,
-      left: 0, 
-      behavior: 'smooth',
+      left: 0,
+      behavior: "smooth",
     });
-  }
-  const upScroll = () => {
-    window.scroll({
-      top: document.body.offsetTop,
-      left: 0, 
-      behavior: 'smooth',
-    });
-  }
+  };
   
+  function toggleVisible() {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 200) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+  function bottomVisible() {
+    const scrolledbottom = document.documentElement.scrollHeight;
+    if (scrolledbottom < 200) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  function csrollBottom(){
+    window.scrollTo({
+      bottom: 0,
+      behavior: "smooth",
+    });
+  }
+
+  window.addEventListener("scroll", toggleVisible);
+  window.addEventListener("scrollbottom", bottomVisible);
+
   return (
     <div>
-      <button onClick={upScroll}  className="button-up">
+      <button
+        className={`${"btn"} ${"scrollBtn"} ${visiable ? "visiable" : ""}`}
+        onClick={scrollToTop}
+      >
         <FaArrowCircleUp />
       </button>
-      <button
-        onClick={downScroll}
-        className="button-down"
-      >
+      <button onClick={downScroll} className="button-down">
         <FaArrowCircleDown />
       </button>
     </div>
