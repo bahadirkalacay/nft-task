a7a70d13d25b5b9a0d1079d810b1bea751e211fd
Bahadır Kalaçay <bahadirkalacay@gmail.com>
2022-12-13 15:06:47 +0300

    context

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ScrollButtons from "./components/ScrollButtons/ScrollButtons";
 
-
 function App() {
-  // window.addEventListener(
-  //   "contextmenu",
-  //   function (e) {
-  //     e.preventDefault();
-  //   },
-  //   false
-  // );
+  window.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
 
   return (
     <div>
-      <ScrollButtons/>
+      <ScrollButtons />
       <Header />
       <Content />
       <Footer />
